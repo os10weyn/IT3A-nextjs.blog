db0c474c613114426202c6250176122623119fdb
os10weyn <111478174+os10weyn@users.noreply.github.com>
2022-08-28 14:20:22 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
-import '../styles/globals.css'
+// `pages/_app.js`
+import '../styles/global.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
